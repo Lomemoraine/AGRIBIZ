a33a2de0e93e17ccbeb5343b2b9feaab5bdcc5cc
Lomemoraine <lorrainechepkemoi2001@gmail.com>
2025-08-28 11:25:07 +0300

    initial commit

---
+++ src/main/java/com/example/agribiz/AgriBizApplication.java
@@ -0,0 +1,13 @@
+package com.example.agribiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgriBizApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AgriBizApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/agribiz/AgriBizApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.agribiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgriBizApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
