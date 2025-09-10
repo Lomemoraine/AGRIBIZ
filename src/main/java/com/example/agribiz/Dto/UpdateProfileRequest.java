d8af8807b3d842f4b73841b0e8e4919e52e13997
Lomemoraine <lorrainechepkemoi2001@gmail.com>
2025-09-10 12:26:48 +0300

    add update profile request dto

---
+++ src/main/java/com/example/agribiz/Dto/UpdateProfileRequest.java
@@ -0,0 +1,19 @@
+package com.example.agribiz.Dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class UpdateProfileRequest {
+    private String firstName;
+    private String lastName;
+    private String nationalId;
+    private String address;
+    private String phoneNumber;
+    private String bio;
+}
