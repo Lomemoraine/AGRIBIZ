30d4c7dd4e2cbca9354bfd24b095f81d66359a36
Lomemoraine <lorrainechepkemoi2001@gmail.com>
2025-09-10 12:10:28 +0300

    add userInfo class for Aunthentication Response DTO structure

---
+++ src/main/java/com/example/agribiz/Dto/UserInfo.java
@@ -0,0 +1,23 @@
+package com.example.agribiz.Dto;
+import com.example.agribiz.Model.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserInfo {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private Role role;
+    private String profileImageUrl;
+    private String nationalId;
+    private String address;
+    private String phoneNumber;
+    private String bio;
+}
