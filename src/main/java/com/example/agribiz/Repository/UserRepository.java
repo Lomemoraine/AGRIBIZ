4d4d7c3a294a27131013bd4de24c3373063c0344
Lomemoraine <lorrainechepkemoi2001@gmail.com>
2025-09-10 11:20:51 +0300

    implement userRepository

--- src/main/java/com/example/agribiz/Repository/UserRepository.java
+++ src/main/java/com/example/agribiz/Repository/UserRepository.java
@@ -1,4 +1,22 @@
 package com.example.agribiz.Repository;
 
-public class UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.example.agribiz.Model.User;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+
+    boolean existsByEmail(String email);
+
+    Optional<User> findByResetToken(String resetToken);
+
+    @Query("SELECT u FROM User u WHERE u.resetToken = :token AND u.resetTokenExpiry > :now")
+    Optional<User> findByValidResetToken(String token, LocalDateTime now);
 }
